d03043cf9276d16b8971b54a941d4a03d59b566b
root <root@cv3663883.novalocal>
2024-03-27 14:28:09 +0300

    prod conf

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -54,12 +54,14 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER', 'ap3'),
                 'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'ap3').'.pusher.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
-                'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                'port' => env('PUSHER_PORT', 80),
+	    ],
+	    'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0
             ],
-            'client_options' => [
+	    'client_options' => [
+		 'verify' => false,
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
             ],
         ],
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,10 +2,13 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+	<!-- Custom scripts --> 
     @vite('resources/js/app.js')
-    @vite('resources/js/echo.js')
-    @vite('resources/js/bootstrap.js')
-{{--    @vite('resources/css/output.css')--}}
+    <script async src="https://rum.cronitor.io/script.js"></script>
+<script>
+    window.cronitor = window.cronitor || function() { (window.cronitor.q = window.cronitor.q || []).push(arguments); };
+    cronitor('config', { clientKey: '6f145183182994b3635e755062b2ceaa' });
+</script>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
