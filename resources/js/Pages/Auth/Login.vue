fba66063e701e309bea24368830fbe24b11acba8
soyombo <soyombo0@naver.com>
2024-06-16 21:07:38 +0300

    fix auth pages

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,5 +1,6 @@
 <script setup>
-import {Link, useForm} from "@inertiajs/vue3";
+import {Link, useForm, usePage} from "@inertiajs/vue3";
+import {computed} from "vue";
 
 let form = useForm({
     email: '',
@@ -9,18 +10,23 @@ let form = useForm({
 let submit = () => {
     form.post('/auth/signin');
 };
+
+const page = usePage()
+const error = computed(() => page.props.errors);
 </script>
 
 <template>
-    <div class="mt-36 m-auto text-center bg-gray-800 bg-opacity-70 p-12 rounded-lg block shadow">
+    <div class="mt-28 m-auto text-center bg-gray-800 bg-opacity-70 p-12 rounded-lg block shadow">
         <form @submit.prevent="submit">
             <div class="grid gap-4">
                 <p class="text-3xl font-bold bg-gradient-to-r from-sky-300 to-sky-500 bg-clip-text text-transparent">Bichig Chat</p>
+                <label class="text-red-500 text-sm invisible">The password field must be at least 6 characters.</label>
                 <input class="rounded-lg text-sm bg-gray-800 text-white p-3 border border-gray-700" v-model="form.email" name="email" type="email" placeholder="Enter email">
                 <input class="rounded-lg text-sm bg-gray-800 text-white p-3 border border-gray-700" v-model="form.password" name="password" type="password" placeholder="Enter password">
+                <label class="text-red-500">{{ error.email }}</label>
             </div>
             <div class="grid">
-                <Link href="/auth/register" class="text-white mt-2 hover:text-sky-500 text-sm">Don't have account yet?</Link>
+                <Link href="/auth/register" class="text-white hover:text-sky-500 text-sm">Don't have account yet?</Link>
                 <button class="p-2 px-12 text-white mx-7 mt-2 bg-sky-500 rounded-2xl hover:bg-sky-700" type="submit">Sign In</button>
             </div>
         </form>
--- resources/js/Pages/User/Page.vue
+++ resources/js/Pages/User/Page.vue
@@ -1,13 +1,21 @@
 <script setup>
 import {Link, usePage} from "@inertiajs/vue3";
 import { computed } from 'vue';
+import axios from "axios";
 
 const page = usePage()
 const user = computed(() => page.props.auth.user);
+
+const logout = () => {
+    axios.get("/auth/logout")
+}
 </script>
 
 <template>
     <p class="text-white">{{ user.name }}</p>
+    <div class="mt-24 m-auto text-center bg-gray-800 bg-opacity-70 p-12 rounded-lg block shadow">
+        <button type="button" v-on:click="logout" class="p-5 bg-sky-400 rounded-2xl hover:bg-sky-700 text-white">Log Out</button>
+    </div>
 </template>
 
 <style scoped>
