d7b9f8792d244d75d8ff714bfa121def09631c92
soyombo <soyombo0@naver.com>
2024-06-24 20:09:27 +0300

    fix routes

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\MessageEvent;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
@@ -9,5 +10,11 @@ class ChatController extends Controller
     public function index()
     {
         return inertia('Chat/Chat');
+    }
+
+    public function sendMessage(Request $request)
+    {
+        broadcast(new MessageEvent($request->message))->toOthers();
+        return to_route('chat.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,14 @@
 <?php
 
-use App\Events\MessageEvent;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ChatController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Client\Request;
 
-Route::get('/', function () {
-    return inertia('Home');
-})->name('about');
-
-Route::post('/button', function (\Illuminate\Http\Request $request) {
-    broadcast(new MessageEvent($request->message))->toOthers();
-    return to_route('chat.index');
-})->name('contact');
-
-Route::get('/contact', function () {
-    return inertia('Contact');
-})->name('contact');
-
-Route::get('/chat', [ChatController::class, 'index'])->name('chat.index');
-
-Route::middleware('auth')->group(function (Router $router) {
-    $router->get('/user', [UserController::class, 'create'])->name('user');
-    $router->put('/user', [UserController::class, 'update'])->name('user.update');
-    $router->post('/user/pic', [UserController::class, 'storePic'])->name('user.pic.store');
-});
-
+// User related routes
 Route::prefix('auth')->group(function () {
     Route::get('/login', [AuthController::class, 'login'])->name('login');
     Route::get('/register', [AuthController::class, 'register'])->name('auth.register');
@@ -36,3 +16,19 @@ Route::prefix('auth')->group(function () {
     Route::post('/signin', [AuthController::class, 'signin'])->name('auth.signin');
     Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 });
+
+Route::middleware('auth')->group(function (Router $router) {
+    $router->get('/user', [UserController::class, 'create'])->name('user');
+    $router->put('/user', [UserController::class, 'update'])->name('user.update');
+    $router->post('/user/pic', [UserController::class, 'storePic'])->name('user.pic.store');
+});
+
+// Home page
+Route::get('/', HomeController::class)->name('home');
+
+// Chat related routes
+Route::get('/chat', [ChatController::class, 'index'])->name('chat.index');
+Route::post('/button', [ChatController::class, 'sendMessage'])->name('send-message');
+
+// Extra routes
+Route::get('/contact', ContactController::class)->name('contact');
